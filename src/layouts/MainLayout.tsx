32444f4d27f12d7b526d9e78a37cf5a4504bc1b5
chandler you <chandler.you@gmail.com>
2023-08-01 11:54:23 +0800

    refine main layout by antd

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,15 +1,23 @@
 import { FC } from "react";
 import { Outlet } from "react-router-dom";
+import { Layout } from "antd";
+import styes from "./MainLayout.module.scss";
 
 const MainLayout: FC = () => {
+  const { Header, Content, Footer } = Layout;
   return (
-    <>
-      <div>MainLayout header</div>
-      <div>
+    <Layout>
+      <Header className={styes.header}>
+        <div className={styes.left}>Logo</div>
+        <div className={styes.right}>Login</div>
+      </Header>
+      <Content className={styes.main}>
         <Outlet></Outlet>
-      </div>
-      <div>MainLayout footer</div>
-    </>
+      </Content>
+      <Footer className={styes.footer}>
+        Survey &copy; 2023 - present. Created by CY
+      </Footer>
+    </Layout>
   );
 };
 
