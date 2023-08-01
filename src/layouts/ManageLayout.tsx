973afd95279032eda8aa0c7ec57a91ec98ed037b
chandler you <chandler.you@gmail.com>
2023-08-01 12:29:11 +0800

    antd: refine manage layout

--- src/layouts/ManageLayout.tsx
+++ src/layouts/ManageLayout.tsx
@@ -1,16 +1,54 @@
 import { FC } from "react";
-import { Outlet } from "react-router-dom";
-import styles from "./Managelayout.module.scss";
+import { Outlet, useNavigate } from "react-router-dom";
+import {
+  PlusOutlined,
+  BarsOutlined,
+  StarOutlined,
+  DeleteOutlined,
+} from "@ant-design/icons";
+import { Button, Space, Divider } from "antd";
+
+import styles from "./ManageLayout.module.scss";
 
 const ManageLayout: FC = () => {
+  const nav = useNavigate();
+
   return (
     <div className={styles.container}>
       <div className={styles.left}>
-        <p>ManageLayout left</p>
-        <button>Create Survey</button>
-        <button>My Survey</button>
-        <button>Star Survey</button>
-        <button>Trash</button>
+        <Space direction="vertical">
+          <Button type="primary" size="large" icon={<PlusOutlined />} block>
+            Create Survey
+          </Button>
+          <Divider style={{ borderTop: "transparent" }} />
+          <Button
+            type="text"
+            size="large"
+            icon={<BarsOutlined />}
+            block
+            onClick={() => nav("/manage/list")}
+          >
+            My Survey
+          </Button>
+          <Button
+            type="text"
+            size="large"
+            icon={<StarOutlined />}
+            block
+            onClick={() => nav("/manage/star")}
+          >
+            Star Survey
+          </Button>
+          <Button
+            type="text"
+            size="large"
+            icon={<DeleteOutlined />}
+            block
+            onClick={() => nav("/manage/trash")}
+          >
+            Trash
+          </Button>
+        </Space>
       </div>
       <div className={styles.right}>
         <Outlet></Outlet>
