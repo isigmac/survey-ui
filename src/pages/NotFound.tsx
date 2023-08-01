7a767b13121b6c080bda276c527a1ab6bd4f17a2
chandler you <chandler.you@gmail.com>
2023-08-01 15:51:45 +0800

    404 page

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,10 +1,23 @@
 import { FC } from "react";
+import { Button, Result } from "antd";
+import { useNavigate } from "react-router-dom";
+
+import { MANAGE_LIST_PATHNAME } from "../router";
 
 const NotFound: FC = () => {
+  const nav = useNavigate();
+
   return (
-    <div>
-      <p>NotFound</p>
-    </div>
+    <Result
+      status="404"
+      title="404"
+      subTitle="Sorry, the page you visited does not exist."
+      extra={
+        <Button type="primary" onClick={() => nav(MANAGE_LIST_PATHNAME)}>
+          Back Home
+        </Button>
+      }
+    />
   );
 };
 
