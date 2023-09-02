c0200a824c47567bfbda89ff6da00d3673e29031
chandler you <chandler.you@gmail.com>
2023-09-02 10:31:08 +0800

    RightPanel: switch tabs automatically

---
+++ src/pages/question/edit/PageSetting.tsx
@@ -0,0 +1,7 @@
+import { FC } from "react";
+
+const PageSetting: FC = () => {
+  return <></>;
+};
+
+export default PageSetting;
--- src/pages/question/edit/RightPanel.tsx
+++ src/pages/question/edit/RightPanel.tsx
@@ -1,14 +1,33 @@
-import { FC } from "react";
+import { FC, useEffect, useState } from "react";
 
 //ui
 import { Tabs } from "antd";
 import { FileTextOutlined, SettingOutlined } from "@ant-design/icons";
 import ComponentProp from "./ComponentProp";
+import PageSetting from "./PageSetting";
+import useGetComponentsInfo from "../../../hooks/useGetComponentsInfo";
+
+enum TAB_KEYS {
+  PROP_KEY = "prop",
+  SETTING_KEY = "setting",
+}
 
 const RightPanel: FC = () => {
+  const { selectedId } = useGetComponentsInfo();
+
+  const [activeKey, setActiveKey] = useState(TAB_KEYS.PROP_KEY);
+
+  useEffect(() => {
+    if (selectedId !== "") {
+      setActiveKey(TAB_KEYS.PROP_KEY);
+    } else {
+      setActiveKey(TAB_KEYS.SETTING_KEY);
+    }
+  }, [selectedId]);
+
   const tabItems = [
     {
-      key: "prop",
+      key: TAB_KEYS.PROP_KEY,
       label: (
         <span>
           <FileTextOutlined /> Properties
@@ -17,19 +36,17 @@ const RightPanel: FC = () => {
       children: <ComponentProp />,
     },
     {
-      key: "setting",
+      key: TAB_KEYS.SETTING_KEY,
       label: (
         <span>
           <SettingOutlined /> Setting
         </span>
       ),
-      children: <div>Setting</div>,
+      children: <PageSetting></PageSetting>,
     },
   ];
 
-  return <Tabs defaultActiveKey="componentLib" items={tabItems}></Tabs>;
-
-  return <Tabs items={tabItems} defaultActiveKey="prop" />;
+  return <Tabs items={tabItems} activeKey={activeKey} />;
 };
 
 export default RightPanel;
